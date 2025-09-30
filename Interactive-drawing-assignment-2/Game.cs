77e08c0645fb88fe0d2c66c94d60ebd3b4c56412
Owen Miller Johnson <000937901@mohawkcollege.ca>
2025-09-30 18:42:58 -0400

    Set up

---
+++ Interactive-drawing-assignment-2/Game.cs
@@ -0,0 +1,35 @@
+﻿// Include the namespaces (code libraries) you need below.
+using System;
+using System.Numerics;
+
+// The namespace your code is in.
+namespace MohawkGame2D
+{
+    /// <summary>
+    ///     Your game code goes inside this class!
+    /// </summary>
+    public class Game
+    {
+        // Place your variables here:
+
+
+        /// <summary>
+        ///     Setup runs once before the game loop begins.
+        /// </summary>
+        public void Setup()
+        {
+            Window.SetTitle("Blinking Eye");
+            Window.SetSize(400, 400);
+            Window.TargetFPS = 60;
+        }
+
+        /// <summary>
+        ///     Update runs every frame.
+        /// </summary>
+        public void Update()
+        {
+
+        }
+    }
+
+}
